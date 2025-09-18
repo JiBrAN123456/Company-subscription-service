593366ed35b4bea70cb0605b94c196cebaf4a6bb
Mohammad Jibran <yunisquadri99@gmail.com>
2025-09-19 01:35:50 +0530

    views done

--- notes_api/settings.py
+++ notes_api/settings.py
@@ -197,6 +197,15 @@ if not os.path.exists(BASE_DIR / 'logs'):
 # Template Configuration for Email Templates
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']  # Update existing TEMPLATES setting
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 
 
